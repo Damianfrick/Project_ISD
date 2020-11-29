c808daef613dcc139d5ab229c55a90141e87a115
Damianfrick <74818655+Damianfrick@users.noreply.github.com>
2020-11-29 18:04:45 +0100

    fixed the error issue / rooting
    imported from the Project instead of the application

--- site_one/routing.py
+++ site_one/routing.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
 
-from Project_ISD import Consumer
+from site_one import Consumer
 
 websocket_urlpatterns=[
     re_path(r'ws/site_one/(?P<person_name>\w+)/$',Consumer.Consumer.as_asgi())

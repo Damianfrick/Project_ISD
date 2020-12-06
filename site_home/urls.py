58617b91de9e36273ac72cbaaf6d4af0ddcfd9a7
Damianfrick <74818655+Damianfrick@users.noreply.github.com>
2020-12-06 23:18:47 +0100

    adding new html for about/contact/news
    added "about, contact and news" the html files / created urls, views

--- site_home/urls.py
+++ site_home/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'site_home'
 urlpatterns = [
     path('',views.index, name='index' ),
+    path('news/',views.news, name='news' ),
+    path('contact/',views.contact, name='contact' ),
+    path('about/',views.about, name='about' ),
 ]
--- site_home/views.py
+++ site_home/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'site_home/base.html')
+
+def about(request):
+    return render(request, 'site_home/about.html')
+
+def news(request):
+    return render(request, 'site_home/news.html')
+
+def contact(request):
+    return render(request, 'site_home/contact.html')

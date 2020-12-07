e2cb3ef6efccc60038e7bdcc5be877e203e84785
Damianfrick <74818655+Damianfrick@users.noreply.github.com>
2020-12-07 18:20:29 +0100

    Created Contact Form
    Allows users to fill our contact form and allows admins to see the posts under the "Contacts" page when logged in.

--- site_home/views.py
+++ site_home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Contact
+from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
@@ -12,4 +13,14 @@ def news(request):
     return render(request, 'site_home/news.html')
 
 def contact(request):
+    if request.method=="POST":
+        contact=Contact()
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        subject=request.POST.get('subject')
+        contact.name=name
+        contact.email=email
+        contact.subject=subject
+        contact.save()
+        return HttpResponse("<h1>THANKS FOR CONTACT US</h1>")
     return render(request, 'site_home/contact.html')

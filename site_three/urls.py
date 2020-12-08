8193cf8e8aa3bd1935fed55fcaab635ccd9fcbd8
Damianfrick <74818655+Damianfrick@users.noreply.github.com>
2020-12-08 13:52:04 +0100

    Creating urls and views for the barchat

--- site_three/urls.py
+++ site_three/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+"""from django.urls import path
 from . import views
 
 app_name = 'site_three'
 urlpatterns = [
     path('',views.index, name='index' ),
+]
+"""
+
+from . import views
+from django.urls import path
+
+urlpatterns=[
+    path('',views.ShowChatHome,name='showchat'),
+    path('room/<str:room_name>/<str:person_name>', views.ShowChatPage, name='showchat'),
 ]
--- site_three/views.py
+++ site_three/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+"""from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return render(request, 'site_three/base.html')
+
+    """
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def ShowChatHome(request):
+    return render(request,"site_three/chat_home.html")
+
+def ShowChatPage(request,room_name,person_name):
+    return render(request,"site_three/chat_screen.html", {'room_name':room_name,'person_name':person_name})

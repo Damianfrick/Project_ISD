34ea85f000be154daae2ae8709f1b8894a5bf7c9
denzel-byte <67588984+denzel-byte@users.noreply.github.com>
2020-12-08 23:22:35 +0100

    Displaying charts on markers
    With this upgrade I am displaying charts for the countries AUT, GER, FL and CH by clicking on the markers.
    As a next step I added information markers with link to the countries information website to coronavirus.
    BugFix to connect positive corona numbers to FL on map

--- site_four/maps_html_creator.py
+++ site_four/maps_html_creator.py
@@ -29,25 +29,61 @@ m.choropleth(
 #Global tooltip
 tooltip ='Click for more Info'
 
-
+#Vega data
+aus = os.path.join('data', 'austria.json')
+ger = os.path.join('data', 'germany.json')
+fli = os.path.join('data', 'liechtenstein.json')
+sch = os.path.join('data', 'switzerland.json')
 
 #Create markers
-folium.Marker([47.149659, 9.516347],
-              popup='<strong><a href="https://www.uni.li/">Visit Uni.li!</a></strong>',
-              icon=folium.Icon(color='red'),
-              tooltip=tooltip).add_to(m)
-
-folium.Marker([47.510081, 14.059615],
+#Info Marker Austria
+folium.Marker([48.214945, 16.417481],
               popup='<strong><a href="https://www.sozialministerium.at/Informationen-zum-Coronavirus/Coronavirus---Aktuelle-Ma%C3%9Fnahmen.html">Corona Info Austria</a></strong>',
               icon=folium.Icon(color='red'),
               tooltip=tooltip).add_to(m)
+
+#Info Marker Germany
+folium.Marker([52.411964, 13.406485],
+              popup='<strong><a href="https://www.bundesgesundheitsministerium.de/coronavirus.html">Corona Info Germany</a></strong>',
+              icon=folium.Icon(color='red'),
+              tooltip=tooltip).add_to(m)
+
+#Info Marker Liechtenstein
+folium.Marker([47.146309, 9.528885],
+              popup='<strong><a href="https://www.regierung.li/coronavirus">Corona Info Liechtenstein</a></strong>',
+              icon=folium.Icon(color='red'),
+              tooltip=tooltip).add_to(m)
+
+#Info Marker Switzerland
+folium.Marker([46.936296, 7.506603],
+              popup='<strong><a href="https://www.bag.admin.ch/bag/de/home/krankheiten/ausbrueche-epidemien-pandemien/aktuelle-ausbrueche-epidemien/novel-cov/situation-schweiz-und-international.html">Corona Info Switzerland</a></strong>',
+              icon=folium.Icon(color='red'),
+              tooltip=tooltip).add_to(m)
+
+
+
+#Austria_New_Cases
+folium.Marker([47.818428, 14.373676],
+              popup=folium.Popup(max_width=450).add_child(folium.Vega(json.load(open(aus))))).add_to(m)
+
+#Germany_New_Cases
+folium.Marker([51.430192, 10.153662],
+              popup=folium.Popup(max_width=450).add_child(folium.Vega(json.load(open(ger))))).add_to(m)
+
+#Liechtenstein_New_Cases
+folium.Marker([47.087587, 9.551893],
+              popup=folium.Popup(max_width=450).add_child(folium.Vega(json.load(open(fli))))).add_to(m)
+
+#Switzerland_New_Cases
+folium.Marker([46.460855, 8.077289],
+              popup=folium.Popup(max_width=450).add_child(folium.Vega(json.load(open(sch))))).add_to(m)
 
 
 
 #Circle
 folium.CircleMarker(
     location=[47.149659, 9.516347],
-    radius=15,
+    radius=4,
     popup='',
     color='#FF0000',
     fill=True,

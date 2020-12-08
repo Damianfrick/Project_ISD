63921b2382dc235dca54d5c82c7fcb44224b53a2
denzel-byte <67588984+denzel-byte@users.noreply.github.com>
2020-12-08 01:57:07 +0100

    Mapping of COVID-19 in Europe
    Inserting new idea on site_four with mapping of current  COVID-19 cases in Europe

---
+++ site_four/maps_html_creator.py
@@ -0,0 +1,37 @@
+import folium
+import pandas as pd
+import os
+
+countries = os.path.join('data', 'countries_world.json')
+covid_data = os.path.join('data', 'Data_use_01.csv')
+state_data = pd.read_csv(covid_data)
+
+#Create map with start location, design and zoom
+m = folium.Map(location=[47,12], tiles='cartodbpositron', zoom_start=5)
+
+
+
+m.choropleth(
+    geo_data=countries,
+    name='choropleth',
+    data=state_data,
+    columns=['iso_code','current_cases'],
+    key_on='feature.id',
+    fill_color='YlOrRd',
+    fill_opacity=0.7,
+    line_opacity=0.0,
+    legend_name='Current COVID-19 cases')
+
+#Global tooltip
+tooltip ='Click for more Info'
+
+#Create markers
+folium.Marker([47.149659, 9.516347],
+              popup='<strong><a href="https://www.uni.li/">Visit Uni.li!</a></strong>',
+              tooltip=tooltip).add_to(m)
+
+
+
+folium.LayerControl().add_to(m)
+#Create map to cd site_four
+m.save('templates/site_four/base.html')

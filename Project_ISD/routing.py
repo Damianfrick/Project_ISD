322f96e4390088834d2e6c3d9fd8cf2c4b3178b5
Damianfrick <74818655+Damianfrick@users.noreply.github.com>
2020-12-08 13:55:08 +0100

    Adding websocket path

--- Project_ISD/routing.py
+++ Project_ISD/routing.py
@@ -1,11 +1,20 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import site_one.routing
+import site_three.routing
 
 application=ProtocolTypeRouter({
     'websocket':AuthMiddlewareStack(
         URLRouter(
             site_one.routing.websocket_urlpatterns
+        )
+    )
+})
+
+application=ProtocolTypeRouter({
+    'websocket':AuthMiddlewareStack(
+        URLRouter(
+            site_three.routing.websocket_urlpatterns
         )
     )
 })

e44ff101324a1e8dc6838476400dbcfe4d485ec6
Damianfrick <74818655+Damianfrick@users.noreply.github.com>
2020-12-15 19:18:14 +0100

    Update Consumer.py
    Added docstrings to "Consumer.py" for better understanding

--- site_one/Consumer.py
+++ site_one/Consumer.py
@@ -11,13 +11,13 @@ class Consumer(WebsocketConsumer):
         self.person_name = self.scope['url_route']['kwargs']['person_name']
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-
+        #Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
 
         )
-
+        #Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
@@ -34,11 +34,12 @@ class Consumer(WebsocketConsumer):
                 "type":"chat_message",
                 "message":self.person_name+" Left Chat"
             }
-        )
+        ) #Leave room group
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
+    #Receive message from room group
     def receive(self, text_data=None, bytes_data=None):
         text_data_json=json.loads(text_data)
         message=text_data_json['message']
@@ -53,7 +54,7 @@ class Consumer(WebsocketConsumer):
 
     def chat_message(self,event):
         message=event['message']
-
+        #send message to Websocket
         self.send(text_data=json.dumps({
             'message':message
         }))
